ece12eb69ac0ae3b559f2b0c648b1df413234998
ukingglobal <Ikenna54@gmail.comQq>
2023-02-24 18:37:10 -0800

    1main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,7 +10,8 @@ int main(void)
 	int i;
 
 	i = 0;
-	positive_or_negative(1);
+
+	positive_or_negative (1);
 
 	return (0);
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,7 +4,6 @@
  * print_number - prints an integer
  * @n: integer to be printed
  */
-
 void print_number(int n)
 {
 	unsigned int n1;
@@ -17,9 +16,12 @@ void print_number(int n)
 	{
 		n1 = n;
 	}
+
 	if (n1 / 10)
 	{
-		print_number(n / 10);
+		print_number(n1 / 10);
 	}
+
 	_putchar((n1 % 10) + '0');
 }
+
